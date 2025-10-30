5df6088b111f9106721e43029f4955f3981f46b6
xThgSilva <camposthiago2021@gmail.com>
2025-10-30 12:15:41 -0300

    feat: Adicionando tela para cadastrar eventos

--- src/components/Eventos/PageAdicionarEvento/index.jsx
+++ src/components/Eventos/PageAdicionarEvento/index.jsx
@@ -1,44 +1,50 @@
-import "../../../styles/Eventos/adicionar-evento/style.css"
+import "../../../styles/Eventos/adicionar-evento/style.css";
 import Button from "../../Button";
+import IconUpload from "../../../assets/Blog/upload.svg";
 
 const AdicionarEvento = () => {
-    return (
-        <>
-            <div className="container-form-evento">
-                <h1 className="titulo-form-evento">Novo evento</h1>
-                <div className="content-form-evento">
-                    <form className="form-evento">
-                        <label htmlFor="nome">Nome</label>
-                        <input name="nome" type="text" placeholder="Nome do evento" />
+  return (
+    <>
+      <div className="container-form-evento">
+        <h1 className="titulo-form-evento">Novo evento</h1>
+        <div className="content-form-evento">
+          <form className="form-evento">
 
-                        {/* Input com data e hora (seletor) */}
+            <label htmlFor="nome">Nome</label>
+            <input name="nome" type="text" placeholder="Nome do evento" />
 
-                        <div className="local-estado">
-                            <label htmlFor="estado">Estado</label>
-                            <input name="estado" type="text" />
+            {/* Input com data e hora (seletor) */}
+            <div className="local-estado">
+              <label htmlFor="estado">Estado</label>
+              <input name="estado" type="text" />
 
-                            <label htmlFor="cidade">Cidade</label>
-                            <input name="cidade" type="text" />
-                        </div>
-
-                        <label htmlFor="endereco">Endereço</label>
-                        <input name="endereco" type="text" />
-
-                        <label htmlFor="regiao">Região</label>
-                        <input name="regiao" type="text" />
-
-                        <label htmlFor="descricao">Descrição para o evento</label>
-                        <textarea name="descricao" id="" placeholder="Digite aqui..."></textarea>
-
-                        {/* Input para colocar a imagem do evento */}
-
-                    </form>
-
-                    <Button text={"Salvar"} />
-                </div>
+              <label htmlFor="cidade">Cidade</label>
+              <input name="cidade" type="text" />
             </div>
-        </>
-    )
-}
+
+            <label htmlFor="endereco">Endereço</label>
+            <input name="endereco" type="text" />
+
+            <label htmlFor="regiao">Região</label>
+            <input name="regiao" type="text" />
+
+            <label htmlFor="descricao">Descrição para o evento</label>
+            <textarea name="descricao" placeholder="Digite aqui..."></textarea>
+
+            {/* Input para colocar a imagem do evento */}
+            <label>Imagem</label>
+            <label htmlFor="uploadImage" className="upload-label">
+              {/* Sem lógica*/}
+              <img id="iconUpload" src={IconUpload} alt="Upload" />
+              <span>Faça o upload da capa ou arraste o arquivo</span>
+            </label>
+
+            <Button className="button" text={"Salvar"} />
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default AdicionarEvento;
